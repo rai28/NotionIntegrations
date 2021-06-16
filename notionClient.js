80edd1464a151ee8e58a366b82085de54ce0a030
Ritesh Rai <riteshrai997@gmail.com>
2021-06-16 11:59:05 +0530

    Forum Created @Notion DB Insert Success ✅

--- notionClient.js
+++ notionClient.js
@@ -4,10 +4,63 @@ const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
 
-async function myDatabase() {
-  const response = await notion.databases.retrieve({
+async function getTags() {
+  const database = await notion.databases.retrieve({
     database_id: process.env.DATABASE_ID,
   });
-  console.log(response);
+  return notionPropertiesById(database.properties)[
+    process.env.NOTION_TAGS_ID
+  ].multi_select.options.map((option) => {
+    return { id: option.id, name: option.name };
+  });
 }
-myDatabase();
+
+function notionPropertiesById(properties) {
+  return Object.values(properties).reduce((obj, property) => {
+    const { id, ...rest } = property;
+    return { ...obj, [id]: rest };
+  }, {});
+}
+
+function createForum({ title, description, tags }) {
+  notion.pages.create({
+    parent: {
+      database_id: process.env.DATABASE_ID,
+    },
+    properties: {
+      [process.env.NOTION_ANIME_NAME_ID]: {
+        title: [
+          {
+            type: "text",
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_DESCRIPTION_ID]: {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: description,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_VOTES_ID]: {
+        number: 0,
+      },
+      [process.env.NOTION_TAGS_ID]: {
+        multi_select: tags.map((tag) => {
+          return { id: tag.id };
+        }),
+      },
+    },
+  });
+}
+
+module.exports = {
+  createForum,
+  getTags,
+};
--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 require("dotenv").config();
 const express = require("express");
-
+const { getTags } = require("./notionClient");
 const app = express();
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+let tags = [];
+getTags().then((data) => {
+  tags = data;
+});
+setInterval(async () => {
+  tags = await getTags();
+}, 1000 * 60 * 60);
+
 const notionClient = require("./notionClient");
 const PORT = 3000;
 app.get("/", (req, res) => {
-  res.send("It WOrks");
+  res.render("index", { tags });
 });
+app.post("/create-forum", async (req, res) => {
+  const { title, description, tagIds = [] } = req.body;
 
+  await notionClient.createForum({
+    title,
+    description,
+    tags: Array.isArray(tagIds)
+      ? tagIds
+      : [tagIds].map((tagId) => {
+          return { id: tagId };
+        }),
+  });
+  res.redirect("/");
+});
 app.listen(PORT);
